76d5fb387a49ac1911b83f17a90eb4982aaf957d
Blake <blake@futchi.com>
2020-04-09 10:35:49 +0200

    updated git ignore, moved model out of db, updated folders, and added factory girl

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const booksController = require('../controllers/booksController')
 
-/* GET books listing. */
+
 router
 .get('/', booksController.index)
 .get('/:id', booksController.show)
